d6d62d85432359230d96234913d971be06d57c20
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-08-08 11:49:36 +0200

    chore: init commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A minimal subset of hercules-ci-effects sufficient for nixbot-effects.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    };
+}
